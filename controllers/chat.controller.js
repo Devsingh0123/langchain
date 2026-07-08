58a1c26bfefad511358ced97b56675ed4b6b5994
Devsingh0123 <dayamns20@gmail.com>
2026-07-08 15:29:11 +0530

    add stringOutputParser for direct content output in response

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,21 +1,24 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import llm from "../config/groq.js";
 import chatPrompt from "../prompts/chat.prompt.js";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 
 export const chatController = async (req, res) => {
   try {
     const { chat_input } = req.body;
 
-    const chain = chatPrompt.pipe(llm);
+    const chain = chatPrompt
+    .pipe(llm)
+    .pipe(new StringOutputParser());
     const response = await chain.invoke({
         question: chat_input,
         name:"jarvis",
         role:"coading"
     });
 
-    res.status(200).json({ response: response.content });
+    res.status(200).json({ response });
   } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500).json({ message: "something went wrong" });
   }
 };
